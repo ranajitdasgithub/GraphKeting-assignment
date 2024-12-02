94d0744a1b9e525618ee7f340e8011ae2e1390f7
Ranajit Das <dasranajit410@gmail.com>
2024-12-03 00:12:48 +0530

    Refactor

--- backend/index.js
+++ backend/index.js
@@ -3,31 +3,27 @@ const cors = require("cors");
 require("dotenv").config();
 const { connection } = require("./config/db");
 const { authentication } = require("./middleware/Authentication");
-const PORT = process.env.PORT || 5000; // Fallback to 5000 if PORT is not set
+const PORT = process.env.PORT || 5000;
 const userRouter = require("./routes/userRoutes");
 const taskRouter = require("./routes/taskRoutes");
 
 const app = express();
 app.use(express.json());
 
-// Correct CORS configuration without NODE_ENV
 const corsOptions = {
   origin: (origin, callback) => {
-    // Check if the request comes from Vercel or local frontend
-    if (
-      origin === process.env.VARCEL_URL ||
-      origin === "http://localhost:3001"
-    ) {
+    const allowedOrigins = [process.env.VARCEL_URL];
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true, // Allow credentials like cookies or Authorization headers
+  credentials: true,
 };
 
-// Use CORS with the defined options
+app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
